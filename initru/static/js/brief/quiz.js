84ca095cd6e96ff1391dbf34915896a4783e7107
Gayfiev Salavat <nicecube11@ya.ru>
2023-03-09 19:24:01 +0300

    Changed

--- initru/static/js/brief/quiz.js
+++ initru/static/js/brief/quiz.js
@@ -4,30 +4,75 @@ const quizBox = document.getElementById('quiz-box');
 const scoreBox = document.getElementById('score-box');
 const resultBox = document.getElementById('result-box');
 const timerBox = document.getElementById('timerParag');
+const alertContainer = document.getElementById('alertBox');
 
-const activateTimer = (time) => { 
-    if (time.toString().length < 2) { 
-        timerBox.innerHTML = `Время: 0`
+function notifFunction(type, text) { 
+    alertContainer.innerHTML = `
+    <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+        <strong>${text}</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `
+}
+
+
+const activateTimer = (time) => {
+    if (time.toString().length < 2) {
+        timerBox.innerHTML = `0${time}:00`;
+    } else {
+        timerBox.innerHTML = `${time}:00`;
     }
+
+    let minutes = time - 1;
+    let seconds = 60;
+    let displaySeconds;
+    let displayMinutes;
+
+    const timer = setInterval(() => {
+        seconds--
+        if (seconds < 0) {
+            seconds = 59;
+            minutes--
+        };
+        if (minutes.toString().length < 2) {
+            displayMinutes = '0' + minutes;
+        } else {
+            displayMinutes = minutes;
+        };
+        if (seconds.toString().length < 2) {
+            displaySeconds = '0' + seconds;
+        } else { 
+            displaySeconds = seconds
+        };
+        if (minutes === 0 && seconds === 0) { 
+            timerBox.innerHTML = '00:00';
+            setTimeout(() => {
+                clearInterval(timer);
+                notifFunction('success', 'Конец теста.');
+                sendData();
+            }, 500);
+        }
+        timerBox.innerHTML = `${displayMinutes}:${displaySeconds}`;
+    }, 1000);
 };
 
 
 $.ajax({
     type: 'GET',
-    url:`${url}/data`,
-    success:(res)=>{
+    url: `${url}/data`,
+    success: (res) => {
         console.log(res)
         const data = res.data
-        data.forEach(el=>{
+        data.forEach(el => {
             for (const [question, answers] of Object.entries(el)) {
                 quizBox.innerHTML += `
-                <div class = 'mb-2 mt-4'>
+                <div class = 'mb-2 mt-3'>
                     <b>${question}</b>
                 </div>
                 `;
                 answers.forEach(answer => {
                     quizBox.innerHTML += `
-                    <div class = ''>
+                    <div>
                         <input class = "ans" id = '${question}-${answer}' name = '${question}' value = '${answer}' type = 'radio'/>
                         <label for = "${question}">${answer} </label>
                     </div>
@@ -35,8 +80,9 @@ $.ajax({
                 });
             };
         });
+        activateTimer(res.time)
     },
-    errors:(res)=>{
+    errors: (res) => {
         console.log(res)
     },
 });
@@ -52,11 +98,11 @@ const sendData = () => {
     const data = {}
 
     data['csrfmiddlewaretoken'] = csrf[0].value;
-    elements.forEach(el=>{
+    elements.forEach(el => {
         if (el.checked) {
             data[el.name] = el.value
         } else {
-            if (!data[el.name]){
+            if (!data[el.name]) {
                 data[el.name] = null;
             }
         }
@@ -65,20 +111,20 @@ const sendData = () => {
     console.log(data)
 
     $.ajax({
-        type:'POST',
-        url:`${url}/save`,
+        type: 'POST',
+        url: `${url}/save`,
         data: data,
-        success: (res)=>{
+        success: (res) => {
             const results = res.results
             console.log(res)
             quizForm.remove(); // удаление формы
-            
-            scoreBox.innerHTML = `Итог: ${res.passed ? '<b class = "text-success">Вы сдали!</b>' : '<b class = "text-danger">Вы не сдали!</b>'} <br/> Ваш результат: <b>${res.score.toFixed(2)}%</b> <br/> Количество верных ответов: ${res.countAnswers} шт.`;
-            
 
-            results.forEach(res=>{
+            scoreBox.innerHTML = `Итог: ${res.passed ? '<b class = "text-success">Вы сдали!</b>' : '<b class = "text-danger">Вы не сдали!</b>'} <br/> Ваш результат: <b>${res.score.toFixed(2)}%</b> <br/> Количество верных ответов: ${res.countAnswers} шт.`;
+
+
+            results.forEach(res => {
                 const resDiv = document.createElement("div");
-                for (const [question, resp] of Object.entries(res)){
+                for (const [question, resp] of Object.entries(res)) {
                     // console.log(question)
                     // console.log(resp)
 
@@ -89,7 +135,7 @@ const sendData = () => {
                     if (resp == 'not answered') {  // если ответ пустой
                         resDiv.innerHTML += '- нет ответа';
                         resDiv.classList.add('bg-danger');
-                    } else { 
+                    } else {
                         const answer = resp['answered']; // ответ пользователя
                         const correct = resp['correct_answer'] // ответ из бд
 
@@ -108,7 +154,7 @@ const sendData = () => {
             })
 
         },
-        error: (res)=>{
+        error: (res) => {
             console.log(res)
         }
     })
@@ -116,5 +162,6 @@ const sendData = () => {
 
 quizForm.addEventListener('submit', e => {
     e.preventDefault();
+    notifFunction('success', 'Конец теста.');
     sendData()
 })
